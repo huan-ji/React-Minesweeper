4b123be5918e7997a5a7d489dafd77d9dd216bc0
Huan Ji <kkiiji@gmail.com>
2015-12-07 18:39:10 -0800

    complete tile component, add render logic

--- components/tile.jsx
+++ components/tile.jsx
@@ -1,15 +1,37 @@
 var React = require('react');
-💣🚩🚩🚩🚩🚩
+
 var Tile = React.createClass({
+  handleClick: function (e) {
+    e.preventDefault();
+    var options;
+    if (e.altKey) {
+      options = "flagging";
+    } else {
+      options = "revealing";
+    }
+    this.props.update(this.props.tile, options);
+  },
+
   render: function () {
+    var show;
+    var className = "tile";
     var tileStatus = this.props.tile;
     if (tileStatus.explored) {
-
-    } else if (tileStatus.bombed) {
-
+      if (tileStatus.bombed) {
+        show = "💣";
+        className += " bombed";
+      } else if (tileStatus.adjacentBombCount > 0) {
+        show = tileStatus.adjacentBombCount;
+        className += " explored";
+      }
+    } else if (tileStatus.flagged) {
+      show = "🚩";
+      className += " flagged";
+    } else {
+      show = "  ";
     }
     return (
-      <div className="tile">💣</div>
+      <div className={className} onClick={this.handleClick}>{show}</div>
     );
   }
 });
